602185513ab2375a2dce71ea9c2bfb897524bbfc
RatanShreshtha <ratan.shreshtha@gmail.com>
2018-04-03 23:14:33 +0530

    First commit for Workstation Setup playbook.

---
+++ roles/configure/files/config.fish
@@ -0,0 +1,16 @@
+# Config options for bobthefish
+set -g theme_display_vagrant yes
+set -g theme_display_docker_machine no
+set -g theme_display_k8s_context yes
+set -g theme_display_hg yes
+set -g theme_display_user yes
+set -g theme_display_hostname yes
+set -g theme_display_vi no
+set -g theme_title_display_user yes
+set -g theme_title_use_abbreviated_path no
+set -g theme_avoid_ambiguous_glyphs yes
+set -g theme_nerd_fonts yes
+set -g theme_show_exit_status yes
+set -g default_user your_normal_user
+set -g theme_color_scheme solarized-dark
+set -x VIRTUAL_ENV_DISABLE_PROMPT 1
